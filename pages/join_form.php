0bfc04bd3326a4815dc9d01615eecb4d2f072986
Seop-kor <dkdn1004@naver.com>
2021-07-14 18:18:07 +0900

    0714 DB구현 완료

--- pages/join_form.php
+++ pages/join_form.php
@@ -3,10 +3,25 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome || Join</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1" />
+  <title>Zay Shop || Join</title>
+  <!-- Favicon Link -->
+  <link rel="shortcut icon" href="/zay/img/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/zay/img/favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="/zay/img/favicon.ico">
+  <!-- Font Awesome Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+  <!-- Reset CSS Link -->
+  <link rel="stylesheet" href="/zay/css/reset.css">
+  <!-- Main Style CSS Link -->
+  <link rel="stylesheet" href="/zay/css/style.css">
+  <!-- Media Style CSS Link -->
+  <link rel="stylesheet" href="/zay/css/media.css">
 </head>
 <body>
+
+  <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/header.php"; ?>
+
   <form action="/zay/php/insert_mem.php" method="post" name="mem_form" enctype="multipart/form-data">
     <p>아이디 : <input type="text" name="mem_id"></p>
     <!-- 아이디 중복체크 -->
@@ -19,6 +34,9 @@
     <button type="button" id="submit_btn">제출</button>
   </form>
 
+  <!-- jQuery Framework Load -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="/zay/js/jq.main.js"></script>
   <script>
 
     const submitBtn = document.querySelector("#submit_btn");
