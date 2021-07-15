12e4305ce13e6bcb9e97a2e17342aa8df6706a7b
Seop-kor <dkdn1004@naver.com>
2021-07-15 11:22:26 +0900

    0715 회원가입 구현완료

--- index.php
+++ index.php
@@ -112,6 +112,8 @@
       </div>
     </section>
     <!-- End of Categories Section -->
+
+    <?php include $_SERVER["DOCUMENT_ROOT"]."/zay/include/footer.php"; ?>
   </div>
 
   <!-- jQuery Framework Load -->
